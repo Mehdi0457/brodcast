dc67965c7616f065c3cdc0cf566b36ce411445d7
Mehdi0457 <48065343+Mehdi0457@users.noreply.github.com>
2019-03-08 17:43:47 +0100

    Update bc.js

--- bc.js
+++ bc.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#bc | Mehdi_..!#5041`,"http://twitch.tv/S-F")
+client.user.setGame(`$bc | Light Server.`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
